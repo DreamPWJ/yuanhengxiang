beaab09b17a7caea20ae2743cbd506e2bcf01f8f
406798106@qq.com <406798106@qq.com>
2016-12-07 15:56:41 +0800

    元亨祥母婴商城

---
+++ www/js/config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by pwj on 2016/12/7.
+ * 系统接口常量配置
+ */
+var configMod = angular.module("starter.config", []);
+
+configMod.constant("YuanHenXiang", {
+  "name": "YuanHenXiang", //项目名称
+  "debug": false, //调试标示 暂无使用
+  "api": "http://a.yuanhengxiang.com",//接口服务地址  使用
+  'siteUrl': "http://a.yuanhengxiang.com",//仓库地址 暂无使用
+  'imgUrl': "http://f.yuanhengxiang.com",//图片地址 暂无使用
+  'moblileApi':"http://m.yuanhengxiang.com",//手机端服务  使用（分享链接展示调用）
+  'version': '0.1.0' //当前版本号
+});
+
