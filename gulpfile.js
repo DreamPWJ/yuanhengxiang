2732d521cfee331a275d100f233c11b7b9e73bc7
panweiji <406798106@qq.com>
2016-12-13 23:47:15 +0800

    元亨祥母婴商城

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('autoprefixer', function () {
       cascade: true, //是否美化属性值 默认：true 像这样：
       //-webkit-transform: rotate(45deg);
       //        transform: rotate(45deg);
-      remove: true //是否去掉不必要的前缀 默认：true
+      remove: false //是否去掉不必要的前缀 默认：true
     }))
     .pipe(gulp.dest('./www/css'));
 });
