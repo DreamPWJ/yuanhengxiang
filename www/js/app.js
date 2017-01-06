e3616c818c671ec509f5d69375c3559de2af4102
406798106@qq.com <406798106@qq.com>
2017-01-06 13:15:07 +0800

    元亨祥母婴商城

--- www/js/app.js
+++ www/js/app.js
@@ -142,15 +142,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     $ionicConfigProvider.form.toggle('large');
     //原生动画效果统一配置
     $ionicNativeTransitionsProvider.setDefaultOptions({
-      duration: 200 ,// in milliseconds (ms), default 400,
-      slowdownfactor: 4, // overlap views (higher number is more) or no overlap (1), default 4
-      iosdelay: -1, // ms to wait for the iOS webview to update before animation kicks in, default -1
-      androiddelay: -1, // same as above but for Android, default -1
-      winphoknedelay: -1, // same as above but for Windows Phone, default -1,
-      fixedPixelsTop: 0, // the number of pixels of your fixed header, default 0 (iOS and Android)
-      fixedPixelsBottom: 0, // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
-      triggerTransitionEvent: '$ionicView.afterEnter', // internal ionic-native-transitions option
-      backInOppositeDirection: false // Takes over default back transition and state back transition to use the opposite direction transition to go back
+      duration: 200 // in milliseconds (ms), default 400,
+
     });
     $ionicNativeTransitionsProvider.setDefaultTransition({
       type: 'slide',
@@ -160,7 +153,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       type: 'slide',
       direction: 'right'
     });
-    $ionicConfigProvider.views.transition('no');
+    // $ionicConfigProvider.views.transition('no');//禁用ionic动画
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
