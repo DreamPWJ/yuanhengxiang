9368f53e57dc6e1ff08e76b86f7fa98c873383fa
406798106@qq.com <406798106@qq.com>
2017-02-09 16:28:07 +0800

    元亨祥母婴商城

--- www/js/services.js
+++ www/js/services.js
@@ -1667,7 +1667,42 @@ angular.module('starter.services', [])
       }
     }
   })
-
+  .service('CityService', function ($q, $http, YuanHenXiang) { //城市相关的接口
+    return {
+      setLocation: function (params) { //定位设置
+        var deferred = $q.defer();// 声明延后执行，表示要去监控后面的执行
+        var promise = deferred.promise
+        promise = $http({
+          method: 'GET',
+          url: YuanHenXiang.api + "/City/location",
+          params: params
+        }).success(function (data) {
+          deferred.resolve(data);// 声明执行成功，即http请求数据成功，可以返回数据了
+        }).error(function (err) {
+          deferred.reject(err);// 声明执行失败，即服务器返回错误
+        });
+        return promise; // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+      }
+    }
+  })
+  .service('SearchService', function ($q, $http, YuanHenXiang) { //搜索相关的接口
+    return {
+      searchGoods: function (params) { //搜索商品
+        var deferred = $q.defer();// 声明延后执行，表示要去监控后面的执行
+        var promise = deferred.promise
+        promise = $http({
+          method: 'GET',
+          url: YuanHenXiang.api + "/Search/searchGoods",
+          params: params
+        }).success(function (data) {
+          deferred.resolve(data);// 声明执行成功，即http请求数据成功，可以返回数据了
+        }).error(function (err) {
+          deferred.reject(err);// 声明执行失败，即服务器返回错误
+        });
+        return promise; // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+      }
+    }
+  })
   .service('WeiXinService', function ($q, $http, YuanHenXiang) { //微信 JS SDK 接口服务定义
     return {
       isWeiXin: function isWeiXin() { //判断是否是微信内置浏览器
